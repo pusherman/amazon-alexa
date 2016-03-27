32a8116c7302a5ed91d1b41311e453686f1ef6ea
Corey Wilson <corey@commercesignals.com>
2016-03-26 23:07:15 -0400

    created basic response object

--- src/Pusherman/Alexa/Response.php
+++ src/Pusherman/Alexa/Response.php
@@ -3,8 +3,35 @@
 namespace Pusherman\Alexa;
 
 class Response
-{
-  function __construct() {
+{ 
+  
+  public function __construct() {
+    $this->appVersion = '0.1';  
+  }
+
+  /**
+   * Add a text response to send
+   */
+  public function say($text) {
+    $this->responseText = $text;
+  }
+
+  /**
+   * Send the response
+   */
+  public function send($endSession = true) {
+    $speach = [
+      'type' => 'PlainText',
+      'text' => $this->responseText
+    ];
+
+    $response = [
+      'version' => $this->appVersion,
+      'response' => ['outputSpeech' => $speach],
+      'shouldEndSession' => $endSession
+    ];
     
+    header('Content-Type: application/json');
+    print json_encode($response);
   }
 }
